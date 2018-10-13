5d4a29d60b19fdcb0bd5a8dd86bad0c23b02686a
Liron Perel <liron.perel@sapiens.com>
2018-10-13 22:52:49 +0300

    Updates

---
+++ precache-manifest.23c846c363a93d2782a633b65997cb75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4b51990994593933754",
+    "url": "/Movies-Db-React/static/js/runtime~main.d4b51990.js"
+  },
+  {
+    "revision": "16664d0f5ebd141bd1ca",
+    "url": "/Movies-Db-React/static/js/main.16664d0f.chunk.js"
+  },
+  {
+    "revision": "30c116cccca2747cb5d8",
+    "url": "/Movies-Db-React/static/js/1.30c116cc.chunk.js"
+  },
+  {
+    "revision": "16664d0f5ebd141bd1ca",
+    "url": "/Movies-Db-React/static/css/main.ab22ac31.chunk.css"
+  },
+  {
+    "revision": "925eae25eceabc1a5c45b214071e5c5b",
+    "url": "/Movies-Db-React/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/Movies-Db-React/precache-manifest.e6e8172d20deeada759d4921ca04357a.js"
+  "/Movies-Db-React/precache-manifest.23c846c363a93d2782a633b65997cb75.js"
 );
 
 workbox.clientsClaim();
