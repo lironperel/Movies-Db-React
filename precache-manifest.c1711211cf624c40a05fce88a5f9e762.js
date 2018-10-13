5756981afcff91fd51e65bf4fc825e08a2da24ac
Liron Perel <liron.perel@sapiens.com>
2018-10-13 22:28:54 +0300

    Updates

---
+++ precache-manifest.c1711211cf624c40a05fce88a5f9e762.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4b51990994593933754",
+    "url": "/Movies-Db-React/static/js/runtime~main.d4b51990.js"
+  },
+  {
+    "revision": "c6edaa558372f39e407f",
+    "url": "/Movies-Db-React/static/js/main.c6edaa55.chunk.js"
+  },
+  {
+    "revision": "74a46f4af6c60a57b411",
+    "url": "/Movies-Db-React/static/js/1.74a46f4a.chunk.js"
+  },
+  {
+    "revision": "c6edaa558372f39e407f",
+    "url": "/Movies-Db-React/static/css/main.3af58272.chunk.css"
+  },
+  {
+    "revision": "de799e586df65406c726fd887880d99c",
+    "url": "/Movies-Db-React/index.html"
+  }
+];
