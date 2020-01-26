3f661c75c187c315037c54e662aedd3a79d188b8
Liron Perel <Lironp90@gmail.com>
2020-01-26 13:54:35 +0200

    added a few improvements

--- src/App.js
+++ src/App.js
@@ -7,27 +7,31 @@ import Movies from './components/Movies.js';
 import { Navbar, Image } from 'react-bootstrap';
 
 class App extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
 
-    this.state = {}
+    this.state = {};
   }
 
   render() {
     return (
-      <div className="main-app">
+      <div className='main-app'>
         <Navbar>
           <Navbar.Header>
             <Navbar.Brand>
-              <Image src= {logo} className="logo-img"/>
-              <a href="#home">Herolo - Movies DB</a>
+              <Image src={logo} className='logo-img' />
+              <a href='#home'>Movies DB</a>
             </Navbar.Brand>
           </Navbar.Header>
         </Navbar>
 
         <Movies />
-        <div className="footer">
-          <p><a href="mailto:lironp90@gmail.com">© 2018 Liron Perel</a></p>
+        <div className='footer'>
+          <p>
+            <a href='mailto:lironp90@gmail.com'>
+              © {new Date().getFullYear()} Liron Perel
+            </a>
+          </p>
         </div>
       </div>
     );
